3ac94dc45dee61fd84db95c75a2579d95d942b5c
Albina <albinabist1993@mail.ru>
2021-10-01 12:51:55 +0300

    попытки изучить vue

---
+++ src/data/products.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg'
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg'
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg'
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: '/img/radio.jpg'
+  }
+]
